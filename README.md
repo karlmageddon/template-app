d7afb50132fc373c9a0c5a344169e474e6ce8103
Karl Puzon <karl.puzon@gmail.com>
2019-06-08 16:08:19 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A Heroku-deploy ready template that includes Express, React, React Router, and Semantic UI
---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require('path')
+
+const app = express();
+
+// Serve static files from React
+app.use(express.static(path.join(__dirname, 'client/build')))
+
+app.get('/api/test', (req,res) => {
+  const payload = { text: "Express /api/test endpoint is working" }
+
+  res.json(payload)
+
+  console.log('Sent test payload')
+})
+
+const port = process.env.PORT || 5000
+app.listen(port)
+
+console.log(`Express backend is now listening on ${port}`)
